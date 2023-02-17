5554cfa3114f8506dc29c1ea29f9bad12dcdf13d
PavelKotlov <pavelkotlov@gmail.com>
2023-02-16 20:20:53 -0800

    Add the function to return coordinates based on ip location to iss.js using the ipwho.is's api

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
-//     console.log("It didn't work!" , error);
+//     console.log("It didn't work MyIP!" , error);
 //     return;
 //   }
 
-//   console.log('It worked! Returned IP:' , ip);
+//   console.log("It worked MyIP: ", ip);
+//   fetchCoordsByIP(ip, (error, coordinates) => {
+//     if (error) {
+//       console.log("It didn't work! No Coordinates" , error);
+//       return;
+//     }
+  
+//     console.log("It worked: our coordinates", coordinates);
+//   });
+
 // });
--- iss.js
+++ iss.js
@@ -16,4 +16,24 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipwho.is/${ip}?output=json`, (error, response, body) => {
+    if (error) return callback(error, null);
+
+    const jsonObject = JSON.parse(body);
+
+    if (!jsonObject.success) {
+      const msg = `Success status was ${jsonObject.success}. Server message says: ${jsonObject.message} when fetching for IP ${jsonObject.ip}`;
+      callback(Error(msg).message, null);
+      return;
+    }
+
+    callback(null, {latitude: JSON.parse(body).latitude, longitude: JSON.parse(body).longitude});
+    
+  });
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
