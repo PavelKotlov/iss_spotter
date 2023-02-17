1c907d06de4afc54781400732169a7a8245365de
PavelKotlov <pavelkotlov@gmail.com>
2023-02-16 19:40:27 -0800

    Refactored iss.js code made the first if statement inline instead of using curly brackets, and returned the ip value instead of the whole JSON object

--- iss.js
+++ iss.js
@@ -3,10 +3,7 @@ const request = require('request');
 const fetchMyIP = function(callback) {
 
   request('https://api.ipify.org?format=json', (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
+    if (error) return callback(error, null);
 
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
@@ -14,7 +11,7 @@ const fetchMyIP = function(callback) {
       return;
     }
 
-    callback(null, JSON.parse(body));
+    callback(null, JSON.parse(body).ip);
     
   });
 };
